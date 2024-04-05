4aa8bb9b20b98d5f0ccbe65096943049252e27ed
MoeFingers <mbzuiter@gmail.com>
2024-04-04 22:59:34 -0600

    updated controller

--- controllers/bands_controller.js
+++ controllers/bands_controller.js
@@ -5,14 +5,21 @@ const { Band } = db
 module.exports = bands
 
 // FIND ALL BANDS
+// FIND ALL BANDS
 bands.get('/', async (req, res) => {
     try {
-        const foundBands = await Band.findAll()
+        const foundBands = await Band.findAll({
+            order: [ [ 'available_start_time', 'ASC' ] ],
+            where: {
+                name: { [Op.like]: `%${req.query.name ? req.query.name : ''}%` }
+            }
+        })
         res.status(200).json(foundBands)
     } catch (error) {
         res.status(500).json(error)
     }
 })
+
 
 // FIND A SPECIFIC BAND
 bands.get('/:id', async (req, res) => {
