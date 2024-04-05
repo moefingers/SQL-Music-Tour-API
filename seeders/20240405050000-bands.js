ed643bd6f4e2ffc625dcabaad8693faa0ed07e6b
MoeFingers <mbzuiter@gmail.com>
2024-04-04 23:07:38 -0600

    created seeder for bands

---
+++ seeders/20240405050000-bands.js
@@ -0,0 +1,39 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     **/
+    await queryInterface.bulkInsert(
+      "bands",
+      [
+        {
+          band_id: 69,
+          name: "Jingle Jongle",
+          genre: "Jangle",
+          available_start_time: new Date(),
+          end_time: new Date(),
+        },
+        {
+          band_id: 420,
+          name: "Dingle Dongle",
+          genre: "Dangle",
+          available_start_time: new Date(),
+          end_time: new Date(),
+        },
+      ],
+      {}
+    );
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  },
+};
